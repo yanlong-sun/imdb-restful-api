6d1e0219183188bb0c31685b1533835275cee1bf
Yanlong Sun <yanlongsun@Yanlong.local>
2023-03-14 00:12:48 -0700

    wrote views in differnt ways

--- browser/urls.py
+++ browser/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from browser import views
-
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register('movie', views.movieView)
 urlpatterns = [
-    path('', views.movieView.as_view()),
-    path('<slug:id>/', views.movieDetailView.as_view()),
+    # path('', views.movieView.as_view({'get': 'list',
+    #                                   'post': 'create'})),
+    # path('<slug:tconst>/',
+    #      views.movieView.as_view({'get': 'retrive',
+    #                               'put': 'update',
+    #                               'delete': 'destory'})),
 ]
+urlpatterns += router.urls
--- jamiemovie/urls.py
+++ jamiemovie/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/', include('browser.urls'))
+    path('', include('browser.urls'))
 ]

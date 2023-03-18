08db4301ae75363947267ff2e98ab1bb8028b796
Yanlong Sun <yanlongsun@Yanlong.local>
2023-03-17 20:29:16 -0700

    paginated response

--- jamiemovie/settings.py
+++ jamiemovie/settings.py
@@ -85,8 +85,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.AutoSchema',
-
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
 WSGI_APPLICATION = 'jamiemovie.wsgi.application'
